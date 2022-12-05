88241ce5eb962cecab1efe9ecac7505bd6ef482c
Lorenzo Guerrini <lollo.guerrini@gmail.com>
2022-12-05 10:04:10 +0100

    Create Set library

---
+++ Set/SetFunctions.h
@@ -0,0 +1,16 @@
+#ifdef _WIN32
+#define EXPORTFUNC __declspec(dllexport)
+#else
+#define EXPORTFUNC
+#endif
+
+#define INIT_SET_NODE
+#include "SetItem.h"
+
+EXPORTFUNC set_table_t *set_new(const size_t hasmap_size);
+
+EXPORTFUNC set_node_t *set_insert(set_table_t *table, const char *key, const size_t key_len);
+
+EXPORTFUNC set_node_t *set_search(set_table_t *table, const char *key,  const size_t key_len);
+
+EXPORTFUNC set_node_t *set_remove(set_table_t *table, const char *key,  const size_t key_len);
---
+++ Set/SetItem.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "ListItem.h"
+
+typedef struct set_node set_node_t;
+
+typedef struct set_table set_table_t;
+
+#ifdef INIT_SET_NODE
+struct set_node{
+    list_node_t *next;
+    size_t key_len;
+    const char *key;
+};
+
+struct set_table {
+    set_node_t **nodes;
+    size_t hashmap_size;
+};
+#endif
